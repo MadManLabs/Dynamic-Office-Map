ccc1dcc33f8dc918325ec9edccf38a5c824d2a81
Adrian Tundrea <adrian.tundrea@endava.com>
2016-03-27 06:26:07 +0300

    Bug fixes

--- DOMClient/app/src/main/java/com/fsociety/domclient/fragment/FindFriendFragment.java
+++ DOMClient/app/src/main/java/com/fsociety/domclient/fragment/FindFriendFragment.java
@@ -1,5 +1,7 @@
 package com.fsociety.domclient.fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
@@ -14,12 +16,17 @@ import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.ViewById;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @EFragment(R.layout.find_friend_fragment)
 public class FindFriendFragment extends BaseFragment {
 	@ViewById(R.id.searchUsernameInstantAutoCompleteTextView)
 	protected InstantAutoCompleteTextView searchUsernameInstantAutoCompleteTextView;
 	@ViewById(R.id.findFriendButton)
 	protected Button findFriendButton;
+
+	public List<String> usernames = new ArrayList<>();
 
 	public InstantAutoCompleteTextView getSearchUsernameInstantAutoCompleteTextView() {
 		return searchUsernameInstantAutoCompleteTextView;
@@ -32,8 +39,21 @@ public class FindFriendFragment extends BaseFragment {
 
 	@Click(R.id.findFriendButton)
 	protected void onFindFriendButtonClick(View view) {
-		Intent intent = new Intent(getActivity(), UserDetailsActivity_.class);
-		intent.putExtra("username", searchUsernameInstantAutoCompleteTextView.getText().toString());
-		getActivity().startActivity(intent);
+		if (usernames.contains(searchUsernameInstantAutoCompleteTextView.getText().toString())) {
+			Intent intent = new Intent(getActivity(), UserDetailsActivity_.class);
+			intent.putExtra("username", searchUsernameInstantAutoCompleteTextView.getText().toString());
+			getActivity().startActivity(intent);
+		} else {
+			AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this.getActivity());
+			alertDialogBuilder.setIconAttribute(android.R.attr.alertDialogIcon);
+			alertDialogBuilder.setTitle("Form error")
+					.setMessage("Please select an user from the dropdown!").setCancelable(false);
+			alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int id) {
+					dialog.dismiss();
+				}
+			});
+			alertDialogBuilder.show();
+		}
 	}
 }
